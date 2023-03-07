3d566bb825b71f53d9d71211ed7e219683b5fdd8
Gvdien <giangvinhdien.tdtu@gmail.com>
2023-03-07 23:30:44 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = '';
+const giftUrl = 'https://www.facebook.com/Gvdien/';
+const eventName = 'Chúc Mừng 08-03 🍍';
+const titleCard = 'Chúc mừng 08-03🍍🍍🍍';
+const contentCard = 'Nhân ngày 8/3, chúc cậu luôn thành công trong công việc, thuận lợi trong các dự định và luôn hạnh phúc trong tình yêu và cuộc sống.';
+
+const giftImage = 'thobaymau.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
