6031a8d7fd64a4329a8c5b18a99ae47cffe5f27f
Gvdien <giangvinhdien.tdtu@gmail.com>
2023-03-08 00:08:52 +0700

    abc

--- 8-3/config.js
+++ 8-3/config.js
@@ -1,5 +1,5 @@
 const nameGirl = '';
-const giftUrl = 'https://www.facebook.com/Gvdien/';
+const giftUrl = '';
 const eventName = 'Chúc Mừng 08-03';   
 const titleCard = 'Chúc mừng 08-03🍍🍍🍍';
 const contentCard = 'Nhân ngày 8/3, chúc em luôn thành công trong công việc học tập, thuận lợi trong các dự định và luôn hạnh phúc trong tình yêu và cuộc sống nhaa';
--- A004/config.js
+++ A004/config.js
@@ -1,5 +1,5 @@
 const nameGirl = '';
-const giftUrl = 'https://www.facebook.com/Gvdien/';
+const giftUrl = '';
 const eventName = 'Chúc Mừng 08-03 báo A004';   
 const titleCard = 'Chúc mừng 08-03 các báo A004🍍🍍🍍';
 const contentCard = 'Nhân ngày 8/3, chúc các bạn nữ A004 luôn báo, luôn thành công trong công việc, thuận lợi trong các dự định và luôn hạnh phúc trong tình yêu và cuộc sống. Mãi báooooo!!!';
